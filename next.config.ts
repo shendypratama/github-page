67d640e48b784340da51778d75afb44826a73e1c
shendypratama <shendypratama760@gmail.com>
2025-04-04 20:29:41 +0700

    update code

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import { withContentlayer } from 'next-contentlayer2';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default withContentlayer(nextConfig);
